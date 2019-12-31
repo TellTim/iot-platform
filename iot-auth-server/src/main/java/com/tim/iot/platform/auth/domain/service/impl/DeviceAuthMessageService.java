1dea01db24eebded4237640ab81b2e44dbca1bf5
wuj <wuj@hunter.com>
2019-12-31 20:11:20 +0800

    完善注册服务.

--- iot-auth-server/src/main/java/com/tim/iot/platform/auth/domain/service/impl/DeviceAuthMessageService.java
+++ iot-auth-server/src/main/java/com/tim/iot/platform/auth/domain/service/impl/DeviceAuthMessageService.java
@@ -2,11 +2,13 @@ package com.tim.iot.platform.auth.domain.service.impl;
 
 import com.tim.iot.platform.auth.domain.repo.IWebSocketSessionRepo;
 import com.tim.iot.platform.auth.domain.service.IDeviceAuthMessageService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.socket.TextMessage;
 
 import java.io.IOException;
 
+@Slf4j
 public class DeviceAuthMessageService implements IDeviceAuthMessageService {
 
     @Autowired
@@ -14,7 +16,8 @@ public class DeviceAuthMessageService implements IDeviceAuthMessageService {
 
     @Override
     public void sendAuthSuccess(String deviceId, String account,Long authAt) throws IOException {
-        String message = String.format("confim#%s#%s",account,String.valueOf(authAt));
+        String message = String.format("confirm#%s#%s",account,String.valueOf(authAt));
+        log.debug("ddd "+message);
         webSocketSessionRepo.get(deviceId).sendMessage(new TextMessage(message));
     }
 }
