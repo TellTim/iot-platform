c0b036ecd7bf9811c88255f8037c2fe7190f3880
wuj <wuj@hunter.com>
2020-01-08 20:04:46 +0800

    部分MediaType在jsonConvert后导致spring boot admin无法获取到日志.

--- iot-register-server/src/main/java/com/tim/iot/platform/register/global/config/WebConfig.java
+++ iot-register-server/src/main/java/com/tim/iot/platform/register/global/config/WebConfig.java
@@ -62,21 +62,7 @@ public class WebConfig implements WebMvcConfigurer {
         List<MediaType> supportedMediaTypes = new ArrayList<>();
         supportedMediaTypes.add(MediaType.APPLICATION_JSON);
         supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
-        supportedMediaTypes.add(MediaType.APPLICATION_ATOM_XML);
         supportedMediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
-        supportedMediaTypes.add(MediaType.APPLICATION_OCTET_STREAM);
-        supportedMediaTypes.add(MediaType.APPLICATION_PDF);
-        supportedMediaTypes.add(MediaType.APPLICATION_RSS_XML);
-        supportedMediaTypes.add(MediaType.APPLICATION_XHTML_XML);
-        supportedMediaTypes.add(MediaType.APPLICATION_XML);
-        supportedMediaTypes.add(MediaType.IMAGE_GIF);
-        supportedMediaTypes.add(MediaType.IMAGE_JPEG);
-        supportedMediaTypes.add(MediaType.IMAGE_PNG);
-        supportedMediaTypes.add(MediaType.TEXT_EVENT_STREAM);
-        supportedMediaTypes.add(MediaType.TEXT_HTML);
-        supportedMediaTypes.add(MediaType.TEXT_MARKDOWN);
-        supportedMediaTypes.add(MediaType.TEXT_PLAIN);
-        supportedMediaTypes.add(MediaType.TEXT_XML);
         fastConverter.setSupportedMediaTypes(supportedMediaTypes);
 
         // 2.添加fastjson的配置信息，比如: 是否需要格式化返回的json数据
@@ -86,9 +72,8 @@ public class WebConfig implements WebMvcConfigurer {
         // 3.在converter中添加配置信息
         fastConverter.setFastJsonConfig(fastJsonConfig);
         // 4.将converter赋值给HttpMessageConverter
-        HttpMessageConverter<?> converter = fastConverter;
         // 5.返回HttpMessageConverters对象
-        return new HttpMessageConverters(converter);
+        return new HttpMessageConverters((HttpMessageConverter<?>) fastConverter);
     }
 
 }
